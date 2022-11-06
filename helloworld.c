a48fa2eb63e9d26c40d3e029725eea533ea23559
Archisha Shukla <archishashukla@gmail.com>
2022-11-06 16:02:13 +0530

    add

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {      
-    printf("Hello World its is archisha !!! ");
+    printf("Hello World its is archisha");
     printf("new change");
     printf("testing trigs");
     return 0;
